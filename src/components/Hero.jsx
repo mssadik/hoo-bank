1873471e3c4d5f3e037fea6f5ca9e0a1ab24c102
Sadik <anarul6777@gmail.com>
2023-09-22 09:10:28 +0600

    billing section is done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,9 +27,9 @@ const Hero = () => {
                 <img className='w-[100%] h-[100%] relative z-[5]' src={robot} alt="billing" />
             </div>
 
-            <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'> </div>
+            <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink_gradient'> </div>
             <div className='absolute rounded-full z-[1] w-[80%] h-[80%] bottom-40 white__gradient pink__gradient'> </div>
-            <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient pink_gradient'> </div>
+            <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20  pink_gradient'> </div>
 
             <div className={`ss:hidden ${styles.flexCenter}`}>
                 <GetStarted></GetStarted>
