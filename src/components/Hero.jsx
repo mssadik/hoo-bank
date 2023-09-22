64de11e37bbebe6a4293309c633b5ed52ff1189c
Sadik <anarul6777@gmail.com>
2023-09-22 07:38:28 +0600

    stats section is done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
             </div>
 
             <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'> </div>
-            <div className='absolute rounded-full z-[1] w-[80%] h-[80%] bottom-40 white_gradient pink__gradient'> </div>
+            <div className='absolute rounded-full z-[1] w-[80%] h-[80%] bottom-40 white__gradient pink__gradient'> </div>
             <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient pink_gradient'> </div>
 
             <div className={`ss:hidden ${styles.flexCenter}`}>
